0f4e5439d9e08014a28d42ed7626a74c9289ff40
Sophie Reddimalla <sophie.reddimalla@gmail.com>
2017-09-19 13:03:14 +0530

    pep8 errors fixed

--- media.py
+++ media.py
@@ -3,21 +3,21 @@ Media.py Classes for Movie Trailer Website
 """
 
 import webbrowser
+
+
 class Movie:
     """
     Basic Movie Class
     """
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_link):
-        self.title               = movie_title
-        self.storyline           = movie_storyline
-        self.poster_image_url    = poster_image
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_link):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_link
+
     def show_trailer(self):
         """
         Open an window to display video of the trailer
         """
         webbrowser.open(self.trailer_youtube_url)
-
-
-        
-        
